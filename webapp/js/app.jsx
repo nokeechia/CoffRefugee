a2f559768af234a719680247bd38a71aa50281f0
Christopher Chiche <christopher.chiche@gmail.com>
2015-10-03 23:14:26 +0200

    Front-end done for payment screens.

--- webapp/js/app.jsx
+++ webapp/js/app.jsx
@@ -1,4 +1,3 @@
-
 "use strict"
 
 const React = require("react");
@@ -7,6 +6,7 @@ const ButtonGroup = require("react-bootstrap/lib/ButtonGroup");
 const Panel = require("react-bootstrap/lib/Panel");
 const Jumbotron = require("react-bootstrap/lib/Jumbotron");
 const Table = require("react-bootstrap/lib/Table");
+const Modal = require("react-bootstrap/lib/Modal")
 // React components
 
 class Header extends React.Component {
@@ -25,10 +25,11 @@ class Header extends React.Component {
 class BigButton extends React.Component {
    render() {
       return <Button onClick={this.props.handleClick} bsStyle={this.props.bsStyle} bsSize="large" block>
-            {this.props.text}
+            {this.props.children}
          </Button>
    }
 }
+
 BigButton.defaultProps = {
    bsStyle: "primary",
 }
@@ -41,41 +42,81 @@ class ItemButton extends React.Component {
    }
 }
 
+class TransactionConfirmationModal extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         coffeeOnHold:false
+      }
+   }
+   render() {
+      let totalToPay = this.props.total;
+      if(this.state.coffeeOnHold) {
+         totalToPay += this.props.coffeePrice;
+      }
+      let handleYesClick = () => this.setState({coffeeOnHold:true});
+      let handleNoClick = () => this.setState({coffeeOnHold:false});
+      return <Modal show={this.props.show} onHide={this.props.handleClose}>
+         <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title">Confirm Payment</Modal.Title>
+         </Modal.Header>
+         <Modal.Body>
+            <p>Total to pay: <strong>{this.props.total.toFixed(2)} CHF</strong> </p>
+            <p>Would you want to put a coffee on hold for a person in need? </p> 
+            <ButtonGroup>
+               <Button 
+                  bsStyle={this.state.coffeeOnHold?"primary":"default"} 
+                  onClick={handleYesClick}>Yes ({this.props.coffeePrice+'.-'})</Button>
+               <Button 
+                  bsStyle={this.state.coffeeOnHold?"default":"primary"} 
+                  onClick={handleNoClick}>No (0.-)</Button>
+            </ButtonGroup>
+            <br/>
+         </Modal.Body>
+         <Modal.Footer>
+            <Button bsStyle='success'>Pay ({totalToPay.toFixed(2)})</Button>
+            <Button onClick={this.props.handleClose}>Close</Button>
+         </Modal.Footer>
+      </Modal> 
+   }
+}
+TransactionConfirmationModal.defaultProps = {
+   total: 0, 
+   coffeePrice: 2.80,
+   show: false, 
+}
+
 class CurrentTransaction extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         selection: {}
+         selection: {},
+         showConfirmModal: false,
       }
-   }
-   handleSubmit() {
-      console.log('OH YEAH');
-      // Show a popup to propose coupon
-      // Update final price
-      // Submit transaction
-   }
-   formatString(item) {
-      return item.name+' - '+item.price+'.-';
    }
 
-   handleItemSelection(id) {
-      var selection = this.state.selection
-      if(typeof selection[id]==='undefined') {
-         selection[id] = {
-            quantity: 1, 
-         }   
-      } else {
-         selection[id].quantity += 1;
-      }
-      this.setState({selection: selection})
-   } 
    render() {
+      let handleItemSelection = id => {
+         var selection = this.state.selection
+         if(typeof selection[id]==='undefined') {
+            selection[id] = {
+               quantity: 1, 
+            }   
+         } else {
+            selection[id].quantity += 1;
+         }
+         this.setState({selection: selection});
+      } 
+      let formatString = item => (item.name+' - '+item.price+'.-');
+      let handleSubmit = () => this.setState({showConfirmModal: true});
+      let handleCloseModal = () => this.setState({showConfirmModal: false});
+         
       var itemButtons = []; 
       
       for(var itemKey in this.props.availableItems) {
          var item = this.props.availableItems[itemKey];
-         itemButtons.push(<ItemButton key={itemKey} handleClick={this.handleItemSelection.bind(this,itemKey)}>
-            { this.formatString(item) }
+         itemButtons.push(<ItemButton key={itemKey} handleClick={handleItemSelection.bind(null,itemKey)}>
+            { formatString(item) }
          </ItemButton>) 
       }
 
@@ -116,17 +157,22 @@ class CurrentTransaction extends React.Component {
          </div>
          <br/>
          <div>
-            <BigButton handleClick={this.handleSubmit.bind(this)} 
-                       text="Submit transaction"/>
+            <BigButton handleClick={handleSubmit}>Submit transaction</BigButton>
             <br/>
-            <BigButton handleClick={this.props.handleCancel} 
-                       text="Cancel transaction"
-                       bsStyle="danger"/>
+            <BigButton handleClick={this.props.handleCancel} bsStyle="danger">
+               Cancel transaction
+            </BigButton>
          </div>
+         <TransactionConfirmationModal 
+            total={total}
+            show={this.state.showConfirmModal} 
+            handleClose={handleCloseModal}
+            />
       </div>
            
    }
 }
+
 CurrentTransaction.defaultProps = {
    availableItems: {
       123: { name:'Coffee',price:1.80}, 
@@ -141,25 +187,17 @@ class TransactionContainer extends React.Component {
       super(props);
       this.state = { transactionInProgress:false}
    }
-
-   newTransaction() {
-      this.setState({transactionInProgress:true});
-   }
-
-   cancelTransaction() {
-      this.setState({transactionInProgress:false});
-   }
-
    render() {
+      let cancelTransaction = () => this.setState({transactionInProgress:false});
+      let newTransaction = () => this.setState({transactionInProgress:true});
+
       var transactElem = null;
       if(this.state.transactionInProgress) {
          // A transaction is in progress
-         transactElem = <CurrentTransaction handleCancel={this.cancelTransaction.bind(this)} />
+         transactElem = <CurrentTransaction handleCancel={cancelTransaction} />
       } else {
          // Propose to create a new transaction
-          transactElem = <BigButton 
-            handleClick={this.newTransaction.bind(this)} 
-            text='New Transaction'/>
+          transactElem = <BigButton handleClick={newTransaction}>New Transaction</BigButton>
       }
       return <Panel className="semi-transparent">
          {transactElem}
