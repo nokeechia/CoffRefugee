97dfe0b561a6d692e2ef100d364c8c83a1cd225d
Keech Achara <keech.achara@thomsonreuters.com>
2015-10-04 00:28:11 +0200

    fixed the never ending json errors by making it a dict

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 __author__ = 'Keech'
 
-from flask import Flask
+from flask import Flask, request
 from Models.Session import Session
 import os
 import json
 from suds.client import Client
 from settings import APP_ROOT, openYaml
 import yaml
+from urllib import error
+
 
 app = Flask(__name__)
 Session = Session()
+currentItemCount = 0
 with openYaml() as stream:
     data = yaml.load(stream)['wsdlUrl']
 aUrl = data["aUrl"]
-aClient = Client(aUrl)
+
+while True:
+    try:
+        aClient = Client(aUrl)
+        break
+    except (TimeoutError, error.URLError) as e:
+        pass
+    print("Client has started successfully")
 tUrl = data["tUrl"]
-tClient= Client(tUrl)
+while True:
+    try:
+        tClient= Client(tUrl)
+        break
+    except (TimeoutError, error.URLError) as e:
+        pass
+    print("Client has started successfully")
 
 
 @app.route('/')
@@ -34,13 +50,32 @@ def login():
 
 @app.route('/getMerchants')
 def getMerchants():
-    merchants = Session.getMerchants(aClient)
+    b = False
+    if currentItemCount > 0:
+        b = True
+    merchants = Session.getMerchants(aClient, b)
     ms =[]
     s = None
     for m in merchants:
-        s = json.dumps(m.__dict__)
+        print(m.__class__)
+        s = json.dumps(m)
         ms.append(s)
     return json.dumps(ms)
+
+@app.route('/addTransaction', methods=['POST'])
+def addTransaction():
+    print("addTransaction")
+    byte = request.get_data()
+    print(byte)
+    data = byte.decode('ascii')
+    print(data)
+    print(data.__class__)
+    rh = json.loads(data)[0]
+    c = {'status': False}
+    currentItemCount = len(rh.keys())
+    if currentItemCount  > 0:
+        c['status'] = True
+    return json.dumps(c)
 
 if __name__ == '__main__':
     app.debug = True
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import json
 from Models import Customer
 
 __author__ = 'Keech'
@@ -7,3 +8,78 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def openYaml():
     return open(os.path.join(APP_ROOT,'Config.yaml'),'r')
 
+def returnMerchantJSON():
+    merchants = {"merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//6f2e002d-c6f1-4bc9-9f7d-d4f538c7058b_thumb.jpg",
+   "merchantLongitude": "8.488552",
+   "merchantName":"kkiosk",
+   "merchantId":"V1",
+   "merchantLatitude":"47.391478",
+   "couponsAvailable":"24",
+   "couponsGiven":"3"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//0ead5c3b-b5af-4e88-a916-c7334708d6a4_thumb.jpg",
+   "merchantLongitude":"8.536542",
+   "merchantName":"Avec",
+   "merchantId":"V2",
+   "merchantLatitude":"47.378991",
+   "couponsAvailable":"3",
+   "couponsGiven":"18"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//03be2fbc-8fb0-4d6b-929a-90dac8ea6e2d_thumb.png",
+   "merchantLongitude":"8.539072",
+   "merchantName":"Brezelk\u00f6nig",
+   "merchantId":"V3",
+   "merchantLatitude":"47.378950",
+   "couponsAvailable":"10",
+   "couponsGiven":"45"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//0c71a43c-3a54-47db-915c-2bf7bf6550ea_thumb.jpg",
+   "merchantLongitude":"8.523853",
+   "merchantName":"Caff\u00e8 Spettacolo",
+   "merchantId":"V4",
+   "merchantLatitude":"47.371149",
+   "couponsAvailable":"14",
+   "couponsGiven":"24"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//6f2e002d-c6f1-4bc9-9f7d-d4f538c7058b_thumb.jpg",
+   "merchantLongitude":"8.549087",
+   "merchantName":"kkiosk",
+   "merchantId":"V1",
+   "merchantLatitude":"47.386028",
+   "couponsAvailable":"7",
+   "couponsGiven":"12"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//6f2e002d-c6f1-4bc9-9f7d-d4f538c7058b_thumb.jpg",
+   "merchantLongitude":"8.498772",
+   "merchantName":"kkiosk",
+   "merchantId":"V1",
+   "merchantLatitude":"47.386663",
+   "couponsAvailable":"2",
+   "couponsGiven":"32"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//0ead5c3b-b5af-4e88-a916-c7334708d6a4_thumb.jpg",
+   "merchantLongitude":"8.540334",
+   "merchantName":"Avec",
+   "merchantId":"V2",
+   "merchantLatitude":"47.378232",
+   "couponsAvailable":"19",
+   "couponsGiven":"10"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//03be2fbc-8fb0-4d6b-929a-90dac8ea6e2d_thumb.png",
+   "merchantLongitude":"8.548686",
+   "merchantName":"Brezelk\u00f6nig",
+   "merchantId":"V3",
+   "merchantLatitude":"47.366703",
+   "couponsAvailable":"23",
+   "couponsGiven":"5"
+},{
+   "merchantLogoThumbnail":"http://www.axiomwebservices.com//MeritImages//0c71a43c-3a54-47db-915c-2bf7bf6550ea_thumb.jpg",
+   "merchantLongitude":", 8.498515",
+   "merchantName":"Caff\u00e8 Spettacolo",
+   "merchantId":"V4",
+   "merchantLatitude":"47.385907",
+   "couponsAvailable":"14",
+   "couponsGiven":"48"
+}
+
+    return merchants
